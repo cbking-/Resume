1d7b51546a626fc4c8806381d5391939f5b21ea9
Corbin King <corbinking90@gmail.com>
2018-02-18 20:25:58 -0500

    Updated resume and mpdf version.
    Added more details for Emprise and changed wording for TH Plastics and BGSU. Reordered skills.
    
    Updated project to use Composer and removed mdpf files from repository.

--- create.php
+++ create.php
@@ -1,7 +1,14 @@
 <?php
-require_once("./mpdf/vendor/autoload.php");
+require_once("./vendor/autoload.php");
 
-$mpdf = new mPDF("utf-8", "Letter", "", "trebuchet", 12.7, 12.7, 12.7, 12.7);
+$mpdf = new \Mpdf\Mpdf(['mode' => 'utf-8', 
+						'format' => 'Letter', 
+						'default_font' => 'trebuchet',
+						'default_font_size' => 10, 
+						'margin_left' => 12.7, 
+						'margin_right' => 12.7,
+						'margin_top' => 12.7,
+						'margin_bottom' => 12.7]);
 
 $css  = file_get_contents("style.css");
 $html = file_get_contents("resume.html");
@@ -11,7 +18,14 @@ $mpdf->WriteHTML($html, 2);
 
 $mpdf->Output("Corbin_King_Resume.pdf", "F");
 
-$mpdf2 = new mPDF("utf-8", "Letter", "", "trebuchet", 12.7, 12.7, 12.7, 12.7);
+$mpdf2 = new \Mpdf\Mpdf(['mode' => 'utf-8', 
+						'format' => 'Letter', 
+						'font' => 'trebuchet', 
+						'margin_left' => 12.7, 
+						'margin_right' => 12.7,
+						'margin_top' => 12.7,
+						'margin_bottom' => 12.7]);
+
 $html = file_get_contents("cover_letter.html");
 
 $mpdf2->WriteHTML($css, 1);
